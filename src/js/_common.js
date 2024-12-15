f95038cfc689e1d824500fbc2f79d730eaef5842
sassy <luxeadd@gmail.com>
2024-12-15 17:08:20 +0900

    form関連追加

--- src/js/_common.js
+++ src/js/_common.js
@@ -10,3 +10,18 @@ if (jsPageTopBtn) {
     getScrolled() > 1000 ? jsPageTopBtn.classList.add('is-active') : jsPageTopBtn.classList.remove('is-active');
   };
 }
+
+// ----------------------
+// ペッダー表示切り替え
+// ----------------------
+let jsHeader = document.querySelector('.js-header');
+let jsHeaderTarget = document.querySelector('.js-headerTarget');
+if (jsHeader) {
+  function getScrolled() {
+    return window.pageYOffset !== undefined ? window.pageYOffset : document.documentElement.scrollTop;
+    jsHeaderTarget.offsetTop;
+  }
+  window.onscroll = function () {
+    getScrolled() > jsHeaderTarget.offsetTop ? jsHeader.classList.add('is-active') : jsHeader.classList.remove('is-active');
+  };
+}
